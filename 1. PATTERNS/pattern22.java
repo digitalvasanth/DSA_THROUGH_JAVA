d24d04e8cd44669b735fc3c4681e1eae0d4c6841
Vasanth Kumar <digitalvasanth01@gmail.com>
2024-08-08 20:53:12 +0530

    commit done

---
+++ 1. PATTERNS/pattern22.java
@@ -0,0 +1,24 @@
+
+
+public class pattern22 {
+    
+    public static void main(String[] args)
+    {
+        int n=5;
+
+        for(int i=0;i<2*n;i++)
+        {
+            for(int j=0;j<2*n;j++)
+            {
+                if((i/2+j/2)%2==0)
+               System.out.print("*");
+               else
+               System.out.print("-");
+
+               
+
+            }
+            System.out.println();
+        }
+    }
+}
